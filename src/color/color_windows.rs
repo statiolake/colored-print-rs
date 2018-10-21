11a23046760da1e7e13e01eeb165ec5697e9aa4c
statiolake <statiolake@gmail.com>
2018-10-22 04:38:06 +0900

    use Stream type just before using HANDLE

--- src/color/color_windows.rs
+++ src/color/color_windows.rs
@@ -10,21 +10,28 @@ use super::ConsoleColor as CC;
 use Stream;
 
 lazy_static! {
-    static ref STDOUT_DEFAULT_ATTRIBUTE: WORD = get_attributes_for(winbase::STD_OUTPUT_HANDLE);
-    static ref STDERR_DEFAULT_ATTRIBUTE: WORD = get_attributes_for(winbase::STD_ERROR_HANDLE);
+    static ref STDOUT_DEFAULT_ATTRIBUTE: WORD = get_attributes_for(Stream::Stdout);
+    static ref STDERR_DEFAULT_ATTRIBUTE: WORD = get_attributes_for(Stream::Stderr);
     static ref STDOUT_DEFAULT_BACKGROUND_ATTRIBUTE: WORD =
         *STDOUT_DEFAULT_ATTRIBUTE & !raw_fg_color(true, true, true, true);
     static ref STDERR_DEFAULT_BACKGROUND_ATTRIBUTE: WORD =
         *STDERR_DEFAULT_ATTRIBUTE & !raw_fg_color(true, true, true, true);
 }
 
-fn get_attributes_for(stream_type: u32) -> WORD {
-    let csbi = unsafe {
+fn get_attributes_for(stream: Stream) -> WORD {
+    unsafe {
         let mut csbi: wincon::CONSOLE_SCREEN_BUFFER_INFO = mem::zeroed();
-        wincon::GetConsoleScreenBufferInfo(processenv::GetStdHandle(stream_type), &mut csbi);
-        csbi
+        wincon::GetConsoleScreenBufferInfo(get_stream_handle_for(stream), &mut csbi);
+        csbi.wAttributes
+    }
+}
+
+fn get_stream_handle_for(stream: Stream) -> HANDLE {
+    let raw_stream = match stream {
+        Stream::Stdout => winbase::STD_OUTPUT_HANDLE,
+        Stream::Stderr => winbase::STD_ERROR_HANDLE,
     };
-    csbi.wAttributes
+    unsafe { processenv::GetStdHandle(raw_stream) }
 }
 
 fn raw_fg_color(red: bool, green: bool, blue: bool, int: bool) -> WORD {
@@ -36,81 +43,64 @@ fn raw_fg_color(red: bool, green: bool, blue: bool, int: bool) -> WORD {
     red | green | blue | int
 }
 
-fn bg_default(stream: HANDLE) -> WORD {
+fn bg_default(stream: Stream) -> WORD {
     unsafe {
-        if stream == processenv::GetStdHandle(winbase::STD_OUTPUT_HANDLE) {
-            *STDOUT_DEFAULT_BACKGROUND_ATTRIBUTE
-        } else if stream == processenv::GetStdHandle(winbase::STD_ERROR_HANDLE) {
-            *STDERR_DEFAULT_BACKGROUND_ATTRIBUTE
-        } else {
-            unreachable!();
+        match stream {
+            Stream::Stdout => *STDOUT_DEFAULT_BACKGROUND_ATTRIBUTE,
+            Stream::Stderr => *STDERR_DEFAULT_BACKGROUND_ATTRIBUTE,
         }
     }
 }
 
-fn fg_cyan(stream: HANDLE) -> WORD {
+fn fg_cyan(stream: Stream) -> WORD {
     bg_default(stream) | raw_fg_color(false, true, true, false)
 }
 
-fn fg_red(stream: HANDLE) -> WORD {
+fn fg_red(stream: Stream) -> WORD {
     bg_default(stream) | raw_fg_color(true, false, false, false)
 }
 
-fn fg_green(stream: HANDLE) -> WORD {
+fn fg_green(stream: Stream) -> WORD {
     bg_default(stream) | raw_fg_color(false, true, false, false)
 }
 
-fn fg_light_green(stream: HANDLE) -> WORD {
+fn fg_light_green(stream: Stream) -> WORD {
     bg_default(stream) | raw_fg_color(false, true, false, true)
 }
 
-fn fg_light_magenta(stream: HANDLE) -> WORD {
+fn fg_light_magenta(stream: Stream) -> WORD {
     bg_default(stream) | raw_fg_color(true, false, true, true)
 }
 
-fn fg_yellow(stream: HANDLE) -> WORD {
+fn fg_yellow(stream: Stream) -> WORD {
     bg_default(stream) | raw_fg_color(true, true, false, false)
 }
 
-fn fg_light_blue(stream: HANDLE) -> WORD {
+fn fg_light_blue(stream: Stream) -> WORD {
     bg_default(stream) | raw_fg_color(false, false, true, true)
 }
 
-fn fg_reset(stream: HANDLE) -> WORD {
-    unsafe {
-        if stream == processenv::GetStdHandle(winbase::STD_OUTPUT_HANDLE) {
-            *STDOUT_DEFAULT_ATTRIBUTE
-        } else if stream == processenv::GetStdHandle(winbase::STD_ERROR_HANDLE) {
-            *STDERR_DEFAULT_ATTRIBUTE
-        } else {
-            unreachable!();
-        }
+fn fg_reset(stream: Stream) -> WORD {
+    match stream {
+        Stream::Stdout => *STDOUT_DEFAULT_ATTRIBUTE,
+        Stream::Stderr => *STDERR_DEFAULT_ATTRIBUTE,
     }
 }
 
 pub fn print(colorize: bool, stream: Stream, color: CC, body: &str) {
     io::stdout().flush().unwrap();
     io::stderr().flush().unwrap();
-    let handle = get_stream_handle(stream);
-    set_console_color(colorize, handle, color);
+    set_console_color(colorize, stream, color);
     match stream {
         Stream::Stdout => write!(io::stdout(), "{}", body).unwrap(),
         Stream::Stderr => write!(io::stderr(), "{}", body).unwrap(),
     }
     io::stdout().flush().unwrap();
     io::stderr().flush().unwrap();
-    set_console_color(colorize, handle, CC::Reset);
+    set_console_color(colorize, stream, CC::Reset);
 }
 
-fn get_stream_handle(stream: Stream) -> HANDLE {
-    let raw_stream = match stream {
-        Stream::Stdout => winbase::STD_OUTPUT_HANDLE,
-        Stream::Stderr => winbase::STD_ERROR_HANDLE,
-    };
-    unsafe { processenv::GetStdHandle(raw_stream) }
-}
-
-fn set_console_color(colorize: bool, stream: HANDLE, color: CC) {
+fn set_console_color(colorize: bool, stream: Stream, color: CC) {
     let color = Some(color).filter(|_| colorize);
     match color {
         Some(CC::Cyan) => set_console_color_impl(stream, fg_cyan(stream)),
@@ -125,8 +115,8 @@ fn set_console_color(colorize: bool, stream: HANDLE, color: CC) {
     }
 }
 
-fn set_console_color_impl(handle: HANDLE, color: WORD) {
+fn set_console_color_impl(stream: Stream, color: WORD) {
     unsafe {
-        wincon::SetConsoleTextAttribute(handle, color);
+        wincon::SetConsoleTextAttribute(get_stream_handle_for(stream), color);
     }
 }
