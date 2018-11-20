988ec5e35b37641e06cdc4268916efafb07eadea
statiolake <statiolake@gmail.com>
2018-11-20 11:04:14 +0900

    port to 2018: fix macro path

--- src/lib.rs
+++ src/lib.rs
@@ -2,55 +2,55 @@ pub mod color;
 
 #[macro_export]
 macro_rules! common_colored_print_impl {
-    ($colorize:expr, $stream:expr, $color:expr, $fmt:expr $(,$args:expr)*) => (
+    ($colorize:expr, $stream:expr, $color:expr, $fmt:expr $(,$args:expr)*) => {
         $crate::color::print($colorize, $stream, $color, format!($fmt $(,$args)*));
-    )
+    };
 }
 
 #[macro_export]
 macro_rules! common_colored_print {
-    ($colorize:expr, $stream:expr; $($color:expr, $fmt:expr $(,$args:expr)*;)+) => (
+    ($colorize:expr, $stream:expr; $($color:expr, $fmt:expr $(,$args:expr)*;)+) => {
         $(
-            common_colored_print_impl!($colorize, $stream, $color, $fmt $(,$args)*);
+            $crate::common_colored_print_impl!($colorize, $stream, $color, $fmt $(,$args)*);
         )*
         $crate::color::print($colorize, $stream, $crate::color::ConsoleColor::Reset, "");
-    )
+    };
 }
 
 #[macro_export]
 macro_rules! common_colored_println {
     ($colorize:expr, $stream:expr; $($color:expr, $fmt:expr $(,$args:expr)*;)+) => {
-        common_colored_print!($colorize, $stream; $($color, $fmt $(,$args)*;)*);
+        $crate::common_colored_print!($colorize, $stream; $($color, $fmt $(,$args)*;)*);
         $crate::color::print($colorize, $stream, $crate::color::ConsoleColor::Reset, "\n");
     };
 }
 
 #[macro_export]
 macro_rules! colored_print {
-    ($colorize:expr; $($color:expr, $fmt:expr $(,$args:expr)*;)+) => (
-        common_colored_print!($colorize, $crate::Stream::Stdout; $($color, $fmt $(,$args)*;)*);
-    )
+    ($colorize:expr; $($color:expr, $fmt:expr $(,$args:expr)*;)+) => {
+        $crate::common_colored_print!($colorize, $crate::Stream::Stdout; $($color, $fmt $(,$args)*;)*);
+    };
 }
 
 #[macro_export]
 macro_rules! colored_println {
-    ($colorize:expr; $($color:expr, $fmt:expr $(,$args:expr)*;)+) => (
-        common_colored_println!($colorize, $crate::Stream::Stdout; $($color, $fmt $(,$args)*;)*);
-    )
+    ($colorize:expr; $($color:expr, $fmt:expr $(,$args:expr)*;)+) => {
+        $crate::common_colored_println!($colorize, $crate::Stream::Stdout; $($color, $fmt $(,$args)*;)*);
+    };
 }
 
 #[macro_export]
 macro_rules! colored_eprint {
-    ($colorize:expr; $($color:expr, $fmt:expr $(,$args:expr)*;)+) => (
-        common_colored_print!($colorize, $crate::Stream::Stderr; $($color, $fmt $(,$args)*;)*);
-    )
+    ($colorize:expr; $($color:expr, $fmt:expr $(,$args:expr)*;)+) => {
+        $crate::common_colored_print!($colorize, $crate::Stream::Stderr; $($color, $fmt $(,$args)*;)*);
+    };
 }
 
 #[macro_export]
 macro_rules! colored_eprintln {
-    ($colorize:expr; $($color:expr, $fmt:expr $(,$args:expr)*;)+) => (
-        common_colored_println!($colorize, $crate::Stream::Stderr; $($color, $fmt $(,$args)*;)*);
-    )
+    ($colorize:expr; $($color:expr, $fmt:expr $(,$args:expr)*;)+) => {
+        $crate::common_colored_println!($colorize, $crate::Stream::Stderr; $($color, $fmt $(,$args)*;)*);
+    };
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -64,6 +64,7 @@ pub enum Stream {
 mod tests {
     use crate::color::ConsoleColor as CC;
     use crate::Stream;
+    use crate::{colored_eprintln, colored_println};
     #[test]
     fn it_works() {
         colored_println! {
